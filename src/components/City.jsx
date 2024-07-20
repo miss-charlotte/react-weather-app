e3b7c43fe6ad4f0246d34b4d71190c1911ae0c12
Charlotte Akongo <charlotte.akongo@gmail.com>
2024-07-20 19:14:26 +0300

    added changes to City.jsx

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -6,103 +6,131 @@ export default function City() {
   return (
     <>
       <div className="d-flex justify-content-between mt-4">
-        <div>
+        <div className="col-6">
           <h1>Kampala</h1>
-          <p>
-            Saturday 14:19, scattered clouds
-            <br />
-            Humidity: 87%, Wind: 4.12km/h
-          </p>
+        
+            <p>Saturday 14:19, scattered clouds<br/>
+            Humidity: 87%, Wind: 4.12km/h</p>
+        
         </div>
-        <div>
-          <h2>15°C</h2>
+        <div className="currentTemperature">
+          <div className="temperatureIcon position-relative">
+            <ReactAnimatedWeather
+              icon="SLEET"
+              color="black"
+              size={32}
+              animate={true}
+            />
+          </div>
+          <h2 className="position-relative">15</h2>
+          <p className="temperatureDegree">°C</p>
         </div>
       </div>
 
-      <div className="weekDays mt-5">
+      <div className="weekDays">
         <div className="row">
           <div className="col">
             <div className="row">
               <p>Mon</p>
-              <p>
+              <div>
                 <ReactAnimatedWeather
                   icon="CLEAR_DAY"
                   color="black"
                   size={32}
                   animate={true}
                 />
-              </p>
+              </div>
+
+              <p>14° 16°</p>
             </div>
           </div>
           <div className="col">
             <div className="row">
               <p>Tue</p>
-              <ReactAnimatedWeather
-                icon="SLEET"
-                color="black"
-                size={32}
-                animate={true}
-              />
+              <div>
+                <ReactAnimatedWeather
+                  icon="SLEET"
+                  color="black"
+                  size={32}
+                  animate={true}
+                />
+              </div>
+              <p>17° 16°</p>
             </div>
           </div>
           <div className="col">
             <div className="row">
               <p>Wed</p>
-              <ReactAnimatedWeather
-                icon="PARTLY_CLOUDY_NIGHT"
-                color="black"
-                size={32}
-                animate={true}
-              />
+              <div>
+                <ReactAnimatedWeather
+                  icon="PARTLY_CLOUDY_NIGHT"
+                  color="black"
+                  size={32}
+                  animate={true}
+                />
+              </div>
+              <p>18° 16°</p>
             </div>
           </div>
           <div className="col">
             <div className="row">
               <p>Thur</p>
-              <ReactAnimatedWeather
-                icon="PARTLY_CLOUDY_DAY"
-                color="black"
-                size={32}
-                animate={true}
-              />
+              <div>
+                <ReactAnimatedWeather
+                  icon="PARTLY_CLOUDY_DAY"
+                  color="black"
+                  size={32}
+                  animate={true}
+                />
+              </div>
+              <p>17° 16°</p>
             </div>
           </div>
           <div className="col">
             <div className="row">
               <p>Fri</p>
-              <ReactAnimatedWeather
-                icon="WIND"
-                color="black"
-                size={32}
-                animate={true}
-              />
+              <div>
+                <ReactAnimatedWeather
+                  icon="WIND"
+                  color="black"
+                  size={32}
+                  animate={true}
+                />
+              </div>
+              <p>17° 16°</p>
             </div>
           </div>
           <div className="col">
             <div className="row">
               <p>Sat</p>
-              <ReactAnimatedWeather
-                icon="FOG"
-                color="black"
-                size={32}
-                animate={true}
-              />
+              <div>
+                <ReactAnimatedWeather
+                  icon="FOG"
+                  color="black"
+                  size={32}
+                  animate={true}
+                />
+              </div>
+              <p>17° 16°</p>
             </div>
           </div>
           <div className="col">
             <div className="row">
               <p>Sun</p>
-              <ReactAnimatedWeather
-                icon="SNOW"
-                color="black"
-                size={32}
-                animate={true}
-              />
+              <div>
+                <ReactAnimatedWeather
+                  icon="SNOW"
+                  color="black"
+                  size={32}
+                  animate={true}
+                />
+              </div>
+              <p>17° 16°</p>
             </div>
           </div>
         </div>
       </div>
-      <p className="footer mt-5">
+      <p className="footer mt-3">
         This project was coded by{" "}
         <a href="https://github.com/miss-charlotte">Charlotte</a> and is{" "}
         <a href="https://github.com/miss-charlotte/react-weather-app">
